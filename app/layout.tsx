fc7d1a6ba43d5c86962cc924fa2f8e036f943982
Otaiki1 <otaikisadiq2804@gmail.com>
2026-04-07 03:37:43 +0100

    update head tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
+                <meta name="talentapp:project_verification" content="5eda6aae08834f82d82304c6e572c98bfcb6bcf36e21a5051cc1f676de18e7098bc5ea1ab1701d586792a83328ee904f43ebd64efba69f769376e6bb00871b3c" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Josefin+Sans:wght@100..700&display=swap" rel="stylesheet" />
